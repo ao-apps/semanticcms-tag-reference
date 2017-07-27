580c84b1138ab80e6b73242e9dd61183ec8752f3
AO Industries, Inc. <support@aoindustries.com>
2017-07-27 11:47:00 -0500

    Corrected project name in top comment.

--- src/main/java/com/semanticcms/tagreference/TagServlet.java
+++ src/main/java/com/semanticcms/tagreference/TagServlet.java
@@ -1,24 +1,24 @@
 /*
- * semanticcms-google-analytics - Includes the Google Analytics tracking code in SemanticCMS pages.
- * Copyright (C) 2016  AO Industries, Inc.
+ * semanticcms-tag-reference - Generates tag library descriptor documentation for .tld files.
+ * Copyright (C) 2016, 2017  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
  *
- * This file is part of semanticcms-google-analytics.
+ * This file is part of semanticcms-tag-reference.
  *
- * semanticcms-google-analytics is free software: you can redistribute it and/or modify
+ * semanticcms-tag-reference is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * semanticcms-google-analytics is distributed in the hope that it will be useful,
+ * semanticcms-tag-reference is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with semanticcms-google-analytics.  If not, see <http://www.gnu.org/licenses/>.
+ * along with semanticcms-tag-reference.  If not, see <http://www.gnu.org/licenses/>.
  */
 package com.semanticcms.tagreference;
 
