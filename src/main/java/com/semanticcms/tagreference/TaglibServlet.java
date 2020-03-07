db853b3a24522c71cca26927417ce04d86fd92e3
AO Industries, Inc. <support@aoindustries.com>
2020-03-06 18:57:10 -0600

    Activating ao-style in page-scope registry.

--- src/main/java/com/semanticcms/tagreference/TaglibServlet.java
+++ src/main/java/com/semanticcms/tagreference/TaglibServlet.java
@@ -1,6 +1,6 @@
 /*
  * semanticcms-tag-reference - Generates tag library descriptor documentation for .tld files.
- * Copyright (C) 2016, 2017, 2018, 2019  AO Industries, Inc.
+ * Copyright (C) 2016, 2017, 2018, 2019, 2020  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -23,19 +23,22 @@
 package com.semanticcms.tagreference;
 
 import com.aoindustries.servlet.http.Dispatcher;
+import com.aoindustries.style.AoStyle;
 import com.aoindustries.tld.parser.Taglib;
+import com.aoindustries.web.resources.servlet.PageServlet;
+import com.aoindustries.web.resources.servlet.RegistryEE;
 import com.semanticcms.core.model.PageRef;
 import com.semanticcms.core.servlet.CapturePage;
 import java.io.IOException;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.jsp.SkipPageException;
 
-public class TaglibServlet extends HttpServlet {
+public class TaglibServlet extends PageServlet {
 
 	private static final long serialVersionUID = 1L;
 
@@ -63,6 +66,9 @@ public class TaglibServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// We use ao-style directly, activate
+		RegistryEE.Page.get(req).activate(AoStyle.RESOURCE_GROUP);
+
 		Map<String,Object> args = new LinkedHashMap<>();
 		args.put("title", title);
 		args.put("shortTitle", shortTitle);
@@ -74,10 +80,11 @@ public class TaglibServlet extends HttpServlet {
 
 		// TODO: Is there a way to get rid of this forward/include duality?
 		// TODO: Perhaps something clever with the way forward is handled inside of a capture?
+		ServletContext servletContext = getServletContext();
 		if(CapturePage.getCaptureContext(req) == null) {
 			// Forward required so can set content type
 			Dispatcher.forward(
-				getServletContext(),
+				servletContext,
 				JSPX_TARGET,
 				req,
 				resp,
@@ -87,7 +94,7 @@ public class TaglibServlet extends HttpServlet {
 			try {
 				// Include required on capture since forward interrupts the final output
 				Dispatcher.include(
-					getServletContext(),
+					servletContext,
 					JSPX_TARGET,
 					req,
 					resp,
