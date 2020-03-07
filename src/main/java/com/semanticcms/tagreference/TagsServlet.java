25c8759cf514ec61acfece63ae876abdcd8f1fd5
AO Industries, Inc. <support@aoindustries.com>
2020-03-06 18:57:08 -0600

    Activating ao-style in page-scope registry.

--- src/main/java/com/semanticcms/tagreference/TagsServlet.java
+++ src/main/java/com/semanticcms/tagreference/TagsServlet.java
@@ -1,6 +1,6 @@
 /*
  * semanticcms-tag-reference - Generates tag library descriptor documentation for .tld files.
- * Copyright (C) 2016, 2017, 2018, 2019  AO Industries, Inc.
+ * Copyright (C) 2016, 2017, 2018, 2019, 2020  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -23,19 +23,22 @@
 package com.semanticcms.tagreference;
 
 import com.aoindustries.servlet.http.Dispatcher;
+import com.aoindustries.style.AoStyle;
 import com.aoindustries.tld.parser.Taglib;
+import com.aoindustries.web.resources.servlet.PageServlet;
+import com.aoindustries.web.resources.servlet.RegistryEE;
 import com.semanticcms.core.model.ResourceRef;
 import com.semanticcms.core.pages.local.CaptureContext;
 import java.io.IOException;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.jsp.SkipPageException;
 
-public class TagsServlet extends HttpServlet {
+public class TagsServlet extends PageServlet {
 
 	private static final long serialVersionUID = 1L;
 
@@ -51,15 +54,20 @@ public class TagsServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// We use ao-style directly, activate
+		RegistryEE.Page.get(req).activate(AoStyle.RESOURCE_GROUP);
+
 		Map<String,Object> args = new LinkedHashMap<>();
 		args.put("tldRef", tldRef);
 		args.put("taglib", taglib);
+
 		// TODO: Is there a way to get rid of this forward/include duality?
 		// TODO: Perhaps something clever with the way forward is handled inside of a capture?
+		ServletContext servletContext = getServletContext();
 		if(CaptureContext.getCaptureContext(req) == null) {
 			// Forward required so can set content type
 			Dispatcher.forward(
-				getServletContext(),
+				servletContext,
 				JSPX_TARGET,
 				req,
 				resp,
@@ -69,7 +77,7 @@ public class TagsServlet extends HttpServlet {
 			try {
 				// Include required on capture since forward interrupts the final output
 				Dispatcher.include(
-					getServletContext(),
+					servletContext,
 					JSPX_TARGET,
 					req,
 					resp,
