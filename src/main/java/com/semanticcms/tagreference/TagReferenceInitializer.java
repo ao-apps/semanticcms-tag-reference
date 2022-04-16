66ee9a4ccea36c29b87abdc0041cf784c91331fc
AO Industries, Inc <support@aoindustries.com>
2022-04-16 02:02:49 -0500

    Added blank line before package declarations
    Resolves "EmptyLineSeparator: 'package' should be separated from previous line."

--- src/main/java/com/semanticcms/tagreference/TagReferenceInitializer.java
+++ src/main/java/com/semanticcms/tagreference/TagReferenceInitializer.java
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with semanticcms-tag-reference.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.semanticcms.tagreference;
 
 import com.aoapps.collections.AoCollections;
